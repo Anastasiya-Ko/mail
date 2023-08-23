6ed3d0fbb4d333b6f374317292aab6b0e2944393
Anastasiya <agropina_90@mail.ru>
2023-08-23 15:19:58 +0300

    add: скрин покрытия тестами

--- src/main/java/kopylova/mail/ServletInitializer.java
+++ src/main/java/kopylova/mail/ServletInitializer.java
@@ -1,27 +1,24 @@
 package kopylova.mail;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
-@SpringBootApplication
+
 public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
 
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(MailApplication.class);
-//	}
-
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return customizerBuilder(builder);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MailApplication.class);
 	}
 
-	public static SpringApplicationBuilder customizerBuilder(SpringApplicationBuilder builder) {
-		return builder.sources(ServletInitializer.class).bannerMode(Banner.Mode.OFF);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return customizerBuilder(builder);
+//	}
+//
+//	public static SpringApplicationBuilder customizerBuilder(SpringApplicationBuilder builder) {
+//		return builder.sources(ServletInitializer.class).bannerMode(Banner.Mode.OFF);
+//	}
 
 }
